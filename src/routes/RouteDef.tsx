ae52c19adafce6edfc80e3d2908580176a9f0c58
euijin <dmlwls7094@naver.com>
2025-01-08 17:37:43 +0900

    Feat: 라우터 추가

--- src/routes/RouteDef.tsx
+++ src/routes/RouteDef.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import MainPage from "../pages/MainPage";
+import LoginPage from "../pages/LoginPage";
 
 export const AppRouteDef = {
   Main: {
     path: "/",
-    element: <MainPage />,
+    element: <LoginPage />,
   },
 };
