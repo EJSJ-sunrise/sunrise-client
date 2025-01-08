65f13336b58f4fe0f1830ea788c2aafb04939432
euijin <dmlwls7094@naver.com>
2025-01-08 17:37:33 +0900

    Fix: styled에서 tailwind로 초기세팅 변경

--- src/routes/AppScreen.tsx
+++ src/routes/AppScreen.tsx
@@ -1,29 +1,14 @@
-// src/routes/AppScreen.tsx
 import React from "react";
-import styled from "styled-components";
 import { BrowserRouter } from "react-router-dom";
 import AppPages from "./AppPages";
 
-const ViewArea = styled.div`
-  width: 600px;
-  height: 100vh;
-  background: #f5faff;
-  margin: 0 auto; /* 중앙 정렬 */
-  display: flex;
-  flex-direction: column;
-  justify-content: center; /* 세로 중앙 정렬 */
-  align-items: center; /* 가로 중앙 정렬 */
-`;
-
 const AppScreen: React.FC = () => {
   return (
-    <ViewArea>
-      <div className="flex flex-col h-screen border-x border-divider text-text">
-        <BrowserRouter>
-          <AppPages />
-        </BrowserRouter>
-      </div>
-    </ViewArea>
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-gray-100 text-center">
+      <BrowserRouter>
+        <AppPages />
+      </BrowserRouter>
+    </div>
   );
 };
 
