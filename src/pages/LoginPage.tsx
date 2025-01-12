7a260a207090cc71c487e659ef11ac04fed161ce
euijin <dmlwls7094@naver.com>
2025-01-12 18:29:23 +0900

    Fix: 공통 레이아웃 생성에 따른 css 수정

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,8 +2,7 @@ import React from "react";
 
 const LoginPage: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-white text-center max-w-xl mx-auto px-12 pt-2">
-      {/* 텍스트 크기와 위치 조정 및 글씨 색상 변경 */}
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)] bg-white text-center px-12 pt-6">
       <h1
         className="text-lg font-semibold mb-4 leading-relaxed"
         style={{ color: "#F06680" }}
@@ -12,19 +11,14 @@ const LoginPage: React.FC = () => {
         어둠 뒤엔 해가 떠오르듯 감사의 순간을 떠올려보세요
       </h1>
 
-      {/* 메인 로고 */}
       <img src="/MainLogo.svg" alt="Main Logo" className="w-60 h-60 mt-4" />
 
-      {/* 소셜 로그인 버튼 - 크기 조정 및 이미지로만 사용 */}
       <div className="flex flex-col items-center gap-6 mt-8 w-full max-w-sm">
-        {/* 구글 로그인 버튼 이미지 */}
         <img
           src="/GoogleLoginBtn.svg"
           alt="Google Login"
           className="w-72 cursor-pointer"
         />
-
-        {/* 카카오 로그인 버튼 이미지 */}
         <img
           src="/KakaoLoginBtn.svg"
           alt="Kakao Login"
