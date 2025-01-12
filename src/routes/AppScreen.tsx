498401a5e5422fd2888af4b6fa04c7fa198a2428
euijin <dmlwls7094@naver.com>
2025-01-12 18:29:43 +0900

    Fix: 공통 레이아웃 적용

--- src/routes/AppScreen.tsx
+++ src/routes/AppScreen.tsx
@@ -1,14 +1,29 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, useLocation } from "react-router-dom";
+import Header from "../components/common/Header";
 import AppPages from "./AppPages";
+
+const AppLayout: React.FC = () => {
+  const location = useLocation();
+
+  return (
+    <div className="w-full min-h-screen flex justify-center bg-gray-100">
+      <div className="w-full max-w-xl bg-white border border-gray-200 rounded-lg shadow-lg flex flex-col">
+        {location.pathname !== "/login" && <Header />}
+
+        <div className="flex-grow overflow-auto">
+          <AppPages />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const AppScreen: React.FC = () => {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-gray-100 text-center">
-      <BrowserRouter>
-        <AppPages />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <AppLayout />
+    </BrowserRouter>
   );
 };
 
